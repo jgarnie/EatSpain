d00a717f0798e4a9c6344ac7070ca6ae6015beae
Jakub Majer <jakub.majer11@gmail.com>
2020-06-29 16:49:37 +0200

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
-import Navbar from "./components/Navbar/Navbar";
+import MainNavbar from "./components/Navbar/MainNavbar";
 import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <MainNavbar />
       <HomePage />
       <Footer />
     </div>
---
+++ frontend/src/components/Navbar/MainNavbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./MainNavbar.scss"
+
+
+const Navbar = () => {
+  return (
+    <nav className="mainNav">
+      <img className="mainNav__img"
+        src={require("../../img/logo.png")}
+        alt="logo"
+      />
+      <span className="mainNav__text">EatSpain</span>
+      <input className="mainNav__search" type="text" placeholder="Search" />
+      <FontAwesomeIcon icon={faShoppingCart} />
+      <span className="mainNav__cartNum">0</span>
+    </nav>
+  );
+};
+
+export default Navbar;
