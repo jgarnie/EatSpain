a3c3bdf0aa2fcb697976c93a226322a6e5b7d09a
Aidam1 <adam.znamenacek@gmail.com>
2020-06-29 16:53:59 +0200

    project structure added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import CategoryNavbar from "./components/CategoryNavbar/CategoryNavbar";
+import CategoryPage from "./pages/CategoryPage/CategoryPage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <HomePage />
+      <CategoryNavbar />
+      <Switch>
+        <Route component={CategoryPage} path="/category/:categoryName" />
+        <Route component={HomePage} path="/" />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
