99b3be376363d3d3e8706070af4137dcf72371a3
Jakub Majer <jakub.majer11@gmail.com>
2020-06-30 17:15:58 +0200

    navbars

--- frontend/src/pages/HomePage/AboutSection/AboutSection.jsx
+++ frontend/src/pages/HomePage/AboutSection/AboutSection.jsx
@@ -3,14 +3,13 @@ import React from "react";
 const AboutSection = () => {
   return (
     <section>
-      <h1>About us</h1>
+      <h1>EatSpain</h1>
+
+      <img src={require("../../../img/main-page.jpg")} alt="paella"/>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil possimus
         facere totam iure maiores quis odio ea provident autem exercitationem
-        voluptatibus quam veniam fugit, vel mollitia sapiente maxime. Ducimus
-        libero delectus, exercitationem maxime error nihil sapiente cupiditate,
-        sunt architecto debitis ipsum non, corporis deserunt voluptatum sint!
-        Quam excepturi qui cum!
+        voluptatibus quam veniam fugit.
       </p>
     </section>
   );
