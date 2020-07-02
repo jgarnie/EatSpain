95bcf3d62dc01f23aa17fbf56b1af41c28ca1570
Jakub Majer <jakub.majer11@gmail.com>
2020-07-02 11:33:11 +0200

    nav and main

--- frontend/src/pages/HomePage/AboutSection/AboutSection.jsx
+++ frontend/src/pages/HomePage/AboutSection/AboutSection.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import "./AboutSection.scss"
+
 
 const AboutSection = () => {
   return (
-    <section>
-      <h1>EatSpain</h1>
+    <section className="about">
+      <h1 className="about__heading">EatSpain</h1>
 
-      <img src={require("../../../img/main-page.jpg")} alt="paella"/>
-      <p>
+<div className="test">
+<p className="about__text">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil possimus
         facere totam iure maiores quis odio ea provident autem exercitationem
         voluptatibus quam veniam fugit.
       </p>
+
+      <img className="about__mainImg" src={require("../../../img/main.jpg")} alt="paella"/>
+
+</div>
+
+
     </section>
   );
 };
