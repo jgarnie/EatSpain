e5eb259e104c8110c0ed1d9e295a2e379d9ef518
jgarnie <garcia.nieto.joaquin@gmail.com>
2020-07-06 18:10:11 +0200

    finished card with props

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./ProductCard.scss";
 
-const ProductCard = () => {
+const ProductCard = ({key,name,description,image,price}) => {
   return (
-    <div className="product-card">
+    <div className="product-card" id={key}>
       <img
         className="product-card__img"
-        src="https://images-na.ssl-images-amazon.com/images/I/51E36huSj0L._AC_.jpg"
-        alt="rice"
+        src={`/public/images/upload/{image}`}
       />
-      <h3 className="product-card__title">Good rice - very good</h3>
-      <div className="product-card__price">24,49 EUR</div>
+      <h3 className="product-card__title">{name}</h3>
+      <div className="product-card__price">{price} EUR</div>
     </div>
   );
 };
-
 export default ProductCard;
+
+
