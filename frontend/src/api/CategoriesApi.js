9fe595509c0bf668047eca9219f36cbca51fad7e
jgarnie <garcia.nieto.joaquin@gmail.com>
2020-07-09 15:15:32 +0200

    fresh pull

--- frontend/src/api/CategoriesApi.js
+++ frontend/src/api/CategoriesApi.js
@@ -9,5 +9,16 @@ export class CategoriesApi {
     }
     setIsLoading(false);
   };
- 
+  static getCategory = async (setData, setIsLoading,category) => {
+    try {
+      const response = await fetch(`${baseUrl}/api/categories/all?name=${category}`);
+      if (!response.status) throw Error(response.statusText);
+      const data = await response.json();
+      console.log("all categories", data);
+      setData(data);
+      setIsLoading(false);
+    } catch (e) {
+      console.log("There was an error when trying to fetch the categories", e);
+    }
+  };
 }
