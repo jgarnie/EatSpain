92f2bac0805414d44fc942e9ce8dab20a3aa6c05
jgarnie <garcia.nieto.joaquin@gmail.com>
2020-07-16 10:54:58 +0200

    added stiles backend

--- backend/resources/views/show.blade.php
+++ backend/resources/views/show.blade.php
@@ -12,29 +12,39 @@
 
 @endif
 
+<div class="show__wrapp">
+    <div class="show__wrapp__content">
 
+            <h1>{{$item->name}}</h1>
 
-<h1>{{$item->name}}</h1>
+            <span>Product ID: </span>
+            <p>{{$item->id}}</p>
 
-<p>{{$item->id}}</p>
+            <span>Description: </span>
+            <p>{{$item->description}}</p>
 
-<p>{{$item->description}}</p>
+            <span>Price: </span>
+            <p>{{$item->price}}Eur</p>
 
-<p>{{$item->price}}Eur</p>
+            <span>Category: </span>
+            <p>{{$category->name}}</p>
 
-<p>{{$category->name}}</p>
-<img src="/images/uploads/{{$item->image}}" alt="" srcset="">
-<br>
-<a href="/items"><button>back</button></a>
-<a href="/item/{{$item->id}}/edit"><button>edit</button></a>
-<form action="/items/delete" method="post">
-    @csrf
-    <input type="hidden" name="id" value="{{$item->id}}">
-    <label>delete
-    <input type="submit" onclick="return confirm('Are you sure to delete {{$item->name}}?')"></label>
-</form>
-
-
+            <div class="show__wrapp__content__img">
+                <img src="/images/uploads/{{$item->image}}" alt="" srcset="">
+            </div>
+            
+            <div class="show__wrapp__content__btns">
+                <a href="/items"><button>back</button></a>
+                <a href="/item/{{$item->id}}/edit"><button>edit</button></a>
+                <form action="/items/delete" method="post">
+                    @csrf
+                    <input type="hidden" name="id" value="{{$item->id}}">
+                    
+                    <button type="submit" onclick="return confirm('Are you sure to delete {{$item->name}}?')">Delete</button>
+                </form>
+            </div>
+    </div>
+</div>
 @endsection
 
 
