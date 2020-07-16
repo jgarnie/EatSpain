eb939b7a6621d5cbcf939d91075fba95a6e365af
Jakub Majer <jakub.majer11@gmail.com>
2020-07-16 11:21:19 +0200

    jakub touchups

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -4,51 +4,45 @@ import ProductCard from "./../../components/ProductCard/ProductCard.jsx";
 import "./SearchPage.scss";
 import Spinner from "../../components/Spinner/Spinner.jsx";
 
-const SearchPage = ({searchValue, ...restProps}) => {
-
-  const {query} = restProps.match.params  
+const SearchPage = ({ searchValue, ...restProps }) => {
+  const { query } = restProps.match.params;
   const [isLoading, setIsLoading] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
 
+  useEffect(() => {
+    setIsLoading(true);
 
-    useEffect(() => {
-        setIsLoading(true);
-    
-        ProductsApi.getSearchProducts(query,setSearchResults, setIsLoading);
-       
-    }, [query]);
+    ProductsApi.getSearchProducts(query, setSearchResults, setIsLoading);
+  }, [query]);
 
+  const results = searchResults;
 
- const results = searchResults;
-
-    return (
-        <>
-          
-          {isLoading ?  <Spinner/> :
-
-           <>
-                <h3>We have found <span className="SeearchPage__poducts__wrapper--total">{results.length}</span> products</h3> 
-                  <div className="SeearchPage__poducts__wrapper">
-                    {results.map((product, index)=>{
-
-                        return (
-                        
-                          <ProductCard 
-                            key={index}
-                            id={product.id}
-                            name={product.name}
-                            image={product.image}
-                            price={product.price}
-                          />
-                        
-                        ) 
-                    })}
-
-                  </div> 
-            </>
-            }        
-        </>
-        )
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="SearchPage">
+          <h3 className="SearchPage__number">
+            We have found <span>{results.length}</span> products
+          </h3>
+          <div className="SearchPage__products">
+            {results.map((product, index) => {
+              return (
+                <ProductCard
+                  key={index}
+                  id={product.id}
+                  name={product.name}
+                  image={product.image}
+                  price={product.price}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SearchPage;
